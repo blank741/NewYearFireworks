f7acea9a5f42f1cc9640d80aa564b0cfcae5b381
Ethan <3314362710@qq.com>
2026-01-02 13:38:15 +0800

    Half

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <thread>
+#include <chrono>
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main() {
+    cout << endl
+         << "  ***** ****** " << endl
+         << "  *   * *      " << endl
+         << "  *   * *      " << endl
+         << "  *   * *      " << endl
+         << "  ***** *****  " << endl
+         << "    *   *   *  " << endl
+         << "  * *   *   *  " << endl
+         << "  * *** *   *  " << endl
+         << "  * *   *****  " << endl
+         << "  * *   *      " << endl
+         << "  * *   *      " << endl
+         << "  * *** *      " << endl
+         << " ***     ****** " << endl 
+         << endl;
+    this_thread::sleep_for(chrono::seconds(1));
+    system("cls");
+    cout << endl
+         << "      *        " << endl
+         << "       *       " << endl
+         << " ************* " << endl
+         << "               " << endl
+         << "   * *   * *   " << endl
+         << "   *  ***  *   " << endl
+         << "   * *   * *   " << endl
+         << "   *********   " << endl
+         << "       *       " << endl
+         << "  ***********  " << endl
+         << "  *  *   *  *  " << endl
+         << "  * ******* *  " << endl
+         << "  *       ***  " << endl 
+         << endl;
+     cin.get();    
+     return 0;
+}
